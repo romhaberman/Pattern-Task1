129214170621712c202822795fad6e3f381e46c8
Roman Khabarov <habermanrom@gmail.com>
2024-08-20 21:59:50 +1000

    redact build

--- src/test/java/data/DataGenerator.java
+++ src/test/java/data/DataGenerator.java
@@ -1,4 +1,4 @@
-package ru.netology.delivery.data;
+package data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.delivery.data.DataGenerator;
+import data.DataGenerator;
 
 import java.time.Duration;
 
